13afea4be059eeacd2e9774b71d716dc42056fcf
jhongmez <gmezjhon@gmail.com>
2021-08-11 03:48:01 -0500

    Model created

---
+++ src/app/models/character-model.ts
@@ -0,0 +1,11 @@
+export class CharacterModel {
+    id:                     number = 0;
+    nameCharacter:          string;
+    roleCharacter:          string;
+    descriptionCharacter:   string;
+    ageCharacter:           number;
+    personalityCharacter:   string;
+    skillCharacter:         string;
+    createdCharacter:       string;
+    nameCreator:            string;
+}
